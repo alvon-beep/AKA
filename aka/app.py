d4cca763e09cbae983a9cf562e63eeb451c41b6d
HaveMine <havemineproject@gmail.com>
2025-12-18 15:58:44 +0700

    fix

--- aka/app.py
+++ aka/app.py
@@ -156,8 +156,8 @@ if st.button("Jalankan Algoritma"):
     waktu_rec = (end_rec - start_rec) * 1000 # ms
 
     # 4. OUTPUT HASIL
-    st.success(f"✅ **Iteratif:** S{n} = {hasil_iter} | Waktu = {waktu_iter:.6f} ms")
-    st.error(f"🔁 **Rekursif:** S{n} = {hasil_rec} | Waktu = {waktu_rec:.6f} ms")
+    st.success(f"✅ **Iteratif:** S({n}) = {hasil_iter} | Waktu = {waktu_iter:.6f} ms")
+    st.error(f"🔁 **Rekursif:** S({n}) = {hasil_rec} | Waktu = {waktu_rec:.6f} ms")
 
     # 5. VISUALISASI BAR CHART
     st.write("### 📊 Perbandingan Waktu Eksekusi")
